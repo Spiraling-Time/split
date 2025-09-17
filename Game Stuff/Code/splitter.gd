3b73292f99177faccfb4ab41e1a0901c2d18aa3c
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-09-16 20:38:37 -0400

    progress

--- Game Stuff/Code/splitter.gd
+++ Game Stuff/Code/splitter.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends RayCast2D
 
 var startPos: Vector2 = Vector2(-100,-100)
 var endPos: Vector2 = Vector2(100,100)
@@ -7,21 +7,30 @@ var points: Array = []
 
 var speed = 100
 
-var done: bool = false
+var done: bool = true
+
 
 func _ready() -> void:
 	position = startPos
+	target_position = endPos
 
 
 #just get entry and exit points (maybe use raycast instead)
 func _physics_process(delta: float) -> void:
+	
 	if !done:
-		if get_overlapping_bodies().size() >= 1:
-			points.append(position)
-		if position != endPos:
-			global_position = global_position.move_toward(endPos, speed * delta)
-			print(position)
-		else:
-			done = true
-			print(points)
+		force_raycast_update()
+		points.append(get_collision_point())
 		
+		position = endPos
+		target_position = startPos
+		
+		force_raycast_update()
+		points.append(get_collision_point())
+		done = true
+		print(points)
+		
+#CREATE A POLYGON WITH THESE POINTS AND SIZE /2
+
+func _on_timer_timeout() -> void:
+	done = false
